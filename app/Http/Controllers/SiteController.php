f4795ee4da741e50a6e5dd4b5cc98a306398feb6
Сергей Смирнов <serge.mir03@gmail.com>
2020-11-06 00:05:30 +0300

    #0004 - show main menu
    install lavary/laravel-menu

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,6 +6,7 @@ use App\Repositories\MenusRepository;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Lavary\Menu\Menu;
 
 class SiteController extends Controller
 {
@@ -35,6 +36,7 @@ class SiteController extends Controller
         $menu = $this->getMenu();
 
         $navigation = view(env('THEME') . '.navigation')
+            ->with('menu', $menu)
             ->render();
 
         $this->vars = array_add($this->vars, 'navigation', $navigation);
@@ -43,13 +45,25 @@ class SiteController extends Controller
             ->with($this->vars);
     }
 
-    /**
-     * @return mixed
-     */
     protected function getMenu()
     {
         $menu = $this->m_rep->get();
 
-        return $menu;
+        $menu_builder = (new Menu)->make('MyNav', function ($m) use ($menu) {
+            foreach ($menu as $item) {
+                if ($item->parent == 0) {
+                    $m->add($item->title, $item->path)
+                        ->id($item->id);
+                } else {
+                    if ($m->find($item->parent)) {
+                        $m->find($item->parent)
+                            ->add($item->title, $item->path)
+                            ->id($item->id);
+                    }
+                }
+            }
+        });
+
+        return $menu_builder;
     }
 }
--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Lavary\Menu\ServiceProvider::class,
     ],
 
     /*
@@ -202,6 +203,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Menu' => Lavary\Menu\Facade::class,
     ],
 
 ];
