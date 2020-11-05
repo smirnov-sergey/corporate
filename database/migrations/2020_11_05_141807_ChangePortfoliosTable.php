b31f92f605145fe566fa6047807b80bab47435d3
Сергей Смирнов <serge.mir03@gmail.com>
2020-11-05 17:35:03 +0300

    #0001 - create database and tables

---
+++ database/migrations/2020_11_05_141807_ChangePortfoliosTable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('portfolios', function (Blueprint $table) {
+            $table->string('filters_alias');
+            $table->foreign('filters_alias')->references('alias')->on('filters');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('portfolios', function (Blueprint $table) {
+            //
+        });
+    }
+}
