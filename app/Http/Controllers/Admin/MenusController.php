bfc52179aa40cb646bdc60794405c6b351521959
Сергей Смирнов <serge.mir03@gmail.com>
2020-11-13 20:18:37 +0300

    #0018 - create pahe add new menu for admin menus content

--- app/Http/Controllers/Admin/MenusController.php
+++ app/Http/Controllers/Admin/MenusController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
+use App\Filter;
 use App\Repositories\ArticlesRepository;
 use App\Repositories\MenusRepository;
 use App\Repositories\PortfoliosRepository;
@@ -54,11 +56,66 @@ class MenusController extends AdminController
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\View\View
+     * @throws \Throwable
      */
     public function create()
     {
-        //
+        $this->title = 'Новый пункт меню';
+
+        $tmp_menus = $this->getMenus()->roots();
+        // уменьшить коллекцию на 1 элемент при каждом вызове функции
+        $menus = $tmp_menus->reduce(function ($return_menus, $menu) {
+            $return_menus[$menu->id] = $menu->title;
+
+            return $return_menus;
+        }, ['0' => 'Родительский пункт меню']);
+
+        $tmp_articles = $this->a_rep->get(['id', 'title', 'alias']);
+        $articles = $tmp_articles->reduce(function ($return_articles, $article) {
+            $return_articles[$article->alias] = $article->title;
+
+            return $return_articles;
+        }, []);
+
+        $tmp_filters = Filter::select(['id', 'title', 'alias'])->get();
+        $filters = $tmp_filters->reduce(function ($return_filters, $filter) {
+            $return_filters[$filter->alias] = $filter->title;
+
+            return $return_filters;
+        }, ['parent' => 'Раздел портфолио']);
+
+        $tmp_portfolios = $this->p_rep->get(['id', 'alias', 'title']);
+        $portfolios = $tmp_portfolios->reduce(function ($return_portfolios, $portfolio) {
+            $return_portfolios[$portfolio->alias] = $portfolio->title;
+
+            return $return_portfolios;
+        }, []);
+
+        $list = [];
+        $list = array_add($list, '0', 'Не используется');
+        $list = array_add($list, 'parent', 'Раздел блог');
+        $categories = Category::select(['title', 'alias', 'parent_id', 'id'])->get();
+
+        foreach ($categories as $category) {
+            if ($category->parent_id === 0) {
+                $list[$category->title] = [];
+            } else {
+                $list[$categories->where('id', $category->parent_id)->first()->title][$category->alias] = $category->title;
+            }
+        }
+
+        $this->content = view(env('THEME') . '.admin.menus_create_content')
+            ->with([
+                'menus' => $menus,
+                'articles' => $articles,
+                'filters' => $filters,
+                'portfolios' => $portfolios,
+                'categories' => $list,
+            ])
+            ->render();
+
+        return $this->renderOutput();
     }
 
     /**
---
+++ resources/views/pink/admin/menus_create_content.blade.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * @var $menus App\Menu
+ * @var $articles App\Article
+ * @var $filters App\Filter
+ * @var $portfolios App\Portfolio
+ * @var $categories App\Category
+ */
+?>
+
+<div id="content-page" class="content group">
+    <div class="hentry group">
+        {!! Form::open([
+            'url' => (isset($menu->id))
+                ? route('admin.menus.update', ['menus' => $menu->id])
+                : route('admin.menus.store'),
+            'class' => 'contact-form',
+            'method' => 'POST'
+        ]) !!}
+
+        <ul>
+            <li class="text-field">
+                <label for="name-contact-us">
+                    <span class="label">Заголовок:</span>
+                    <br/>
+                    <span class="sublabel">Заголовок пункта</span>
+                </label>
+                <div class="input-prepend">
+                    <span class="add-on">
+                        <i class="icon-user"></i>
+                    </span>
+                    {!! Form::text(
+                       'title',
+                       isset($menu->title) ? $menu->title : old('title'),
+                       ['placeholder' => 'Введите название страницы']
+                    ) !!}
+                </div>
+            </li>
+
+            <li class="text-field">
+                <label for="name-contact-us">
+                    <span class="label">Родительский пункт меню:</span>
+                    <br/>
+                    <span class="sublabel">Родитель</span>
+                </label>
+                <div class="input-prepend">
+                    {!! Form::select(
+                       'parent',
+                       $menus,
+                       isset($menu->title) ? $menu->parent : null
+                    ) !!}
+                </div>
+            </li>
+        </ul>
+
+        <h1>Тип меню:</h1>
+
+        <div id="accordion">
+            <h3>
+                {!! Form::radio('type', 'customLink', (isset($type) && $type == 0) ? '' : '2') !!}
+                <span class="label">Пользовательская ссылка</span>
+            </h3>
+
+            <ul>
+                <li class="text-field">
+                    <label for="name-contact-us">
+                        <span class="label">Путь для ссылки:</span>
+                        <br/>
+                        <span class="sublabel">Путь для ссылки</span>
+                        <br/>
+                    </label>
+                    <div class="input-prepend">
+                        <span class="add-on">
+                          <i class="icon-user"></i>
+                        </span>
+                        {!! Form::text(
+                            'custom_link',
+                            isset($menu->path) ? $menu->path : old('path'),
+                            ['placeholder' => 'Введите путь для ссылки']
+                        ) !!}
+                    </div>
+                </li>
+                <div style="clear:both;"></div>
+            </ul>
+
+            <h3>
+                {!! Form::radio('type', 'blogLink', (isset($type) && $type == 0) ? '' : '2') !!}
+                <span class="label">Раздел блог:</span>
+            </h3>
+
+            <ul>
+                <li class="text-field">
+                    <label for="name-contact-us">
+                        <span class="label">Ссылка на категорию блога:</span>
+                        <br/>
+                        <span class="sublabel">Ссылка на категорию блога</span>
+                    </label>
+                    <div class="input-prepend">
+                        @if($categories)
+                            {!! Form::select(
+                               'category_alias',
+                               $categories,
+                               isset($option) ? $option : null
+                            ) !!}
+                        @endif
+                    </div>
+                </li>
+
+                <li class="text-field">
+                    <label for="name-contact-us">
+                        <span class="label">Ссылка на материал блога:</span>
+                        <br/>
+                        <span class="sublabel">Ссылка на материал блога</span>
+                    </label>
+                    <div class="input-prepend">
+                        @if($articles)
+                            {!! Form::select(
+                               'article_alias',
+                               $articles,
+                               isset($option) ? $option : null
+                            ) !!}
+                        @endif
+                    </div>
+                </li>
+            </ul>
+
+            <h3>
+                {!! Form::radio('type', 'portfolioLink', (isset($type) && $type == 0) ? '' : '2') !!}
+                <span class="label">Раздел портфолио:</span>
+            </h3>
+
+            <ul>
+                <li class="text-field">
+                    <label for="name-contact-us">
+                        <span class="label">Ссылка на запись портфолио:</span>
+                        <br/>
+                        <span class="sublabel">Ссылка на запись портфолио</span>
+                        <br/>
+                    </label>
+                    <div class="input-prepend">
+                        {!! Form::select(
+                            'portfolio_alias',
+                            $portfolios,
+                            isset($option) ? $option : null
+                        ) !!}
+                    </div>
+                </li>
+                <li class="text-field">
+                    <label for="name-contact-us">
+                        <span class="label">Портфолио</span>
+                        <br/>
+                        <span class="sublabel">Портфолио</span>
+                        <br/>
+                    </label>
+                    <div class="input-prepend">
+                        {!! Form::select(
+                            'filter_alias',
+                            $filters,
+                            isset($option) ? $option : null
+                        ) !!}
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <br/>
+
+        @if(isset($menu_id))
+            <input type="hidden" name="_method" value="PUT">
+        @endif
+
+        <ul>
+            <li class="submit-button">
+                {!! Form::button('Сохранить', ['class' => 'btn btn-french-1', 'type' => 'submit']) !!}
+            </li>
+        </ul>
+
+        {!! Form::close() !!}
+    </div>
+</div>
